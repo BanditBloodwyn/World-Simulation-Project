059eb1f5df2fb7fe63cde581737fd70549175615
BanditBloodwyn <kevin@eichenbergs.info>
2022-12-05 17:01:25 +0100

    Kamera + SpeedHUD, PerlinNoiseEvaluator refactored, TMP

--- Assets/_Project/_Scripts/World/Generators/Noise/PerlinNoiseEvaluator.cs
+++ Assets/_Project/_Scripts/World/Generators/Noise/PerlinNoiseEvaluator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Unity.Collections;
+﻿using Unity.Collections;
 using Unity.Mathematics;
 
 namespace Assets._Project._Scripts.World.Generators.Noise
@@ -8,32 +7,27 @@ namespace Assets._Project._Scripts.World.Generators.Noise
     {
         #region Values
 
-        /// Initial permutation table
-        public NativeArray<int> Source;
+        private NativeArray<int> _source;
 
         private const int RandomSize = 256;
-        private int[]? _random;
+        private NativeArray<int> _random;
 
         private const double F3 = 1.0 / 3.0;
         private const double G3 = 1.0 / 6.0;
 
-        /// <summary>
-        /// Gradient vectors for 3D (pointing to mid points of all edges of a unit
-        /// cube)
-        /// </summary>
-        private static readonly int[][] Grad3 =
+        private static readonly NativeArray<NativeArray<int>> Grad3 = new(new NativeArray<int>[]
         {
-            new[] { 1, 1, 0 }, new[] { -1, 1, 0 }, new[] { 1, -1, 0 },
-            new[] { -1, -1, 0 }, new[] { 1, 0, 1 }, new[] { -1, 0, 1 },
-            new[] { 1, 0, -1 }, new[] { -1, 0, -1 }, new[] { 0, 1, 1 },
-            new[] { 0, -1, 1 }, new[] { 0, 1, -1 }, new[] { 0, -1, -1 }
-        };
+            new(new[] { 1, 1, 0 }, Allocator.Persistent),   new(new[] { -1, 1, 0 }, Allocator.Persistent),  new(new[] { 1, -1, 0 }, Allocator.Persistent),
+            new(new[] { -1, -1, 0 }, Allocator.Persistent), new(new[] { 1, 0, 1 }, Allocator.Persistent),   new(new[] { -1, 0, 1 }, Allocator.Persistent),
+            new(new[] { 1, 0, -1 }, Allocator.Persistent),  new(new[] { -1, 0, -1 }, Allocator.Persistent), new(new[] { 0, 1, 1 }, Allocator.Persistent),
+            new(new[] { 0, -1, 1 }, Allocator.Persistent),  new(new[] { 0, 1, -1 }, Allocator.Persistent),  new(new[] { 0, -1, -1 }, Allocator.Persistent)
+        }, Allocator.Persistent);
 
         #endregion
 
         public PerlinNoiseEvaluator(int seed, NativeArray<int> source) : this()
         {
-            Source = source;
+            _source = source;
             Randomize(seed);
         }
 
@@ -47,35 +41,24 @@ namespace Assets._Project._Scripts.World.Generators.Noise
             double z = point.z;
             double n0 = 0, n1 = 0, n2 = 0, n3 = 0;
 
-            // Noise contributions from the four corners
-            // Skew the input space to determine which simplex cell we're in
             double s = (x + y + z) * F3;
 
-            // for 3D
             int i = FastFloor(x + s);
             int j = FastFloor(y + s);
             int k = FastFloor(z + s);
 
             double t = (i + j + k) * G3;
-
-            // The x,y,z distances from the cell origin
             double x0 = x - (i - t);
             double y0 = y - (j - t);
             double z0 = z - (k - t);
 
-            // For the 3D case, the simplex shape is a slightly irregular tetrahedron.
-            // Determine which simplex we are in.
-            // Offsets for second corner of simplex in (i,j,k)
             int i1, j1, k1;
-
-            // coords
-            int i2, j2, k2; // Offsets for third corner of simplex in (i,j,k) coords
+            int i2, j2, k2;
 
             if (x0 >= y0)
             {
                 if (y0 >= z0)
                 {
-                    // X Y Z order
                     i1 = 1;
                     j1 = 0;
                     k1 = 0;
@@ -85,7 +68,6 @@ namespace Assets._Project._Scripts.World.Generators.Noise
                 }
                 else if (x0 >= z0)
                 {
-                    // X Z Y order
                     i1 = 1;
                     j1 = 0;
                     k1 = 0;
@@ -95,7 +77,6 @@ namespace Assets._Project._Scripts.World.Generators.Noise
                 }
                 else
                 {
-                    // Z X Y order
                     i1 = 0;
                     j1 = 0;
                     k1 = 1;
@@ -106,10 +87,8 @@ namespace Assets._Project._Scripts.World.Generators.Noise
             }
             else
             {
-                // x0 < y0
                 if (y0 < z0)
                 {
-                    // Z Y X order
                     i1 = 0;
                     j1 = 0;
                     k1 = 1;
@@ -119,7 +98,6 @@ namespace Assets._Project._Scripts.World.Generators.Noise
                 }
                 else if (x0 < z0)
                 {
-                    // Y Z X order
                     i1 = 0;
                     j1 = 1;
                     k1 = 0;
@@ -129,7 +107,6 @@ namespace Assets._Project._Scripts.World.Generators.Noise
                 }
                 else
                 {
-                    // Y X Z order
                     i1 = 0;
                     j1 = 1;
                     k1 = 0;
@@ -139,97 +116,69 @@ namespace Assets._Project._Scripts.World.Generators.Noise
                 }
             }
 
-            // A step of (1,0,0) in (i,j,k) means a step of (1-c,-c,-c) in (x,y,z),
-            // a step of (0,1,0) in (i,j,k) means a step of (-c,1-c,-c) in (x,y,z),
-            // and
-            // a step of (0,0,1) in (i,j,k) means a step of (-c,-c,1-c) in (x,y,z),
-            // where c = 1/6.
-
-            // Offsets for second corner in (x,y,z) coords
             double x1 = x0 - i1 + G3;
             double y1 = y0 - j1 + G3;
             double z1 = z0 - k1 + G3;
 
-            // Offsets for third corner in (x,y,z)
             double x2 = x0 - i2 + F3;
             double y2 = y0 - j2 + F3;
             double z2 = z0 - k2 + F3;
 
-            // Offsets for last corner in (x,y,z)
             double x3 = x0 - 0.5;
             double y3 = y0 - 0.5;
             double z3 = z0 - 0.5;
 
-            // Work out the hashed gradient indices of the four simplex corners
             int ii = i & 0xff;
             int jj = j & 0xff;
             int kk = k & 0xff;
 
-            // Calculate the contribution from the four corners
             double t0 = 0.6 - x0 * x0 - y0 * y0 - z0 * z0;
             if (t0 > 0)
             {
                 t0 *= t0;
-                if (_random != null)
-                {
-                    int gi0 = _random[ii + _random[jj + _random[kk]]] % 12;
-                    n0 = t0 * t0 * Dot(Grad3[gi0], x0, y0, z0);
-                }
+                int gi0 = _random[ii + _random[jj + _random[kk]]] % 12;
+                n0 = t0 * t0 * Dot(Grad3[gi0], x0, y0, z0);
             }
 
             double t1 = 0.6 - x1 * x1 - y1 * y1 - z1 * z1;
             if (t1 > 0)
             {
                 t1 *= t1;
-                if (_random != null)
-                {
-                    int gi1 = _random[ii + i1 + _random[jj + j1 + _random[kk + k1]]] % 12;
-                    n1 = t1 * t1 * Dot(Grad3[gi1], x1, y1, z1);
-                }
+                int gi1 = _random[ii + i1 + _random[jj + j1 + _random[kk + k1]]] % 12;
+                n1 = t1 * t1 * Dot(Grad3[gi1], x1, y1, z1);
             }
 
             double t2 = 0.6 - x2 * x2 - y2 * y2 - z2 * z2;
             if (t2 > 0)
             {
                 t2 *= t2;
-                if (_random != null)
-                {
-                    int gi2 = _random[ii + i2 + _random[jj + j2 + _random[kk + k2]]] % 12;
-                    n2 = t2 * t2 * Dot(Grad3[gi2], x2, y2, z2);
-                }
+                int gi2 = _random[ii + i2 + _random[jj + j2 + _random[kk + k2]]] % 12;
+                n2 = t2 * t2 * Dot(Grad3[gi2], x2, y2, z2);
             }
 
             double t3 = 0.6 - x3 * x3 - y3 * y3 - z3 * z3;
             if (t3 > 0)
             {
                 t3 *= t3;
-                if (_random != null)
-                {
-                    int gi3 = _random[ii + 1 + _random[jj + 1 + _random[kk + 1]]] % 12;
-                    n3 = t3 * t3 * Dot(Grad3[gi3], x3, y3, z3);
-                }
+                int gi3 = _random[ii + 1 + _random[jj + 1 + _random[kk + 1]]] % 12;
+                n3 = t3 * t3 * Dot(Grad3[gi3], x3, y3, z3);
             }
 
-            // Add contributions from each corner to get the final noise value.
-            // The result is scaled to stay just inside [-1,1]
             return (float)(n0 + n1 + n2 + n3) * 32;
         }
 
         private void Randomize(int seed)
         {
-            _random = new int[RandomSize * 2];
+            _random = new NativeArray<int>(RandomSize * 2, Allocator.Persistent);
 
             if (seed != 0)
             {
-                // Shuffle the array using the given seed
-                // Unpack the seed into 4 bytes then perform a bitwise XOR operation
-                // with each byte
-                var F = new byte[4];
+                NativeArray<byte> F = new NativeArray<byte>(4, Allocator.Temp);
                 UnpackLittleUint32(seed, ref F);
 
-                for (int i = 0; i < Source.Length; i++)
+                for (int i = 0; i < _source.Length; i++)
                 {
-                    _random[i] = Source[i] ^ F[0];
+                    _random[i] = _source[i] ^ F[0];
                     _random[i] ^= F[1];
                     _random[i] ^= F[2];
                     _random[i] ^= F[3];
@@ -241,11 +190,11 @@ namespace Assets._Project._Scripts.World.Generators.Noise
             else
             {
                 for (int i = 0; i < RandomSize; i++)
-                    _random[i + RandomSize] = _random[i] = Source[i];
+                    _random[i + RandomSize] = _random[i] = _source[i];
             }
         }
 
-        private static double Dot(int[] g, double x, double y, double z)
+        private static double Dot(NativeArray<int> g, double x, double y, double z)
         {
             return g[0] * x + g[1] * y + g[2] * z;
         }
@@ -255,17 +204,8 @@ namespace Assets._Project._Scripts.World.Generators.Noise
             return x >= 0 ? (int)x : (int)x - 1;
         }
 
-        /// <summary>
-        /// Unpack the given integer (int32) to an array of 4 bytes  in little endian format.
-        /// If the length of the buffer is too smal, it wil be resized.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <param name="buffer">The output buffer.</param>
-        private static void UnpackLittleUint32(int value, ref byte[] buffer)
+        private static void UnpackLittleUint32(int value, ref NativeArray<byte> buffer)
         {
-            if (buffer.Length < 4)
-                Array.Resize(ref buffer, 4);
-
             buffer[0] = (byte)(value & 0x00ff);
             buffer[1] = (byte)((value & 0xff00) >> 8);
             buffer[2] = (byte)((value & 0x00ff0000) >> 16);
